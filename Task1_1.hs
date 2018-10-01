3ac1b37bd1e30e7238ae498fb5be828a33436df5
Pavel Pokatilo <gradergage@gmail.com>
2018-10-01 22:46:02 +0300

    Task1

--- Task1_1.hs
+++ Task1_1.hs
@@ -4,24 +4,49 @@ import Todo(todo)
 
 data Term = IntConstant{ intValue :: Int }           -- числовая константа
             | Variable{ varName :: String }          -- переменная
-            | BinaryTerm{ lhv :: Term, rhv :: Term } -- бинарная операция
+            | BinaryTerm{ lhv :: Term, rhv :: Term, operation :: Operation } -- бинарная операция
             deriving(Show,Eq)
 
+data Operation = Sum
+                 | Diff
+                 | Mult
+                 deriving (Show, Eq)
+
 -- Для бинарных операций необходима не только реализация, но и адекватные
--- ассоциативность и приоритет
+-- ассоциативность и приоритет	
 (|+|) :: Term -> Term -> Term
-(|+|) l r = todo
+(|+|) l r = BinaryTerm l r Sum
+infixl 5 |+|
+
 (|-|) :: Term -> Term -> Term
-(|-|) l r = todo
+(|-|) l r = BinaryTerm l r Diff
+infixl 5 |-|
+
 (|*|) :: Term -> Term -> Term
-(|*|) l r = todo
+(|*|) l r = BinaryTerm l r Mult
+infixl 6 |*|
+
+
+solveTerm (IntConstant l) (IntConstant r) op  = case op of Sum -> IntConstant(l+r)
+                                                           Diff -> IntConstant(l-r)
+                                                           Mult -> IntConstant(l*r)
+solveTerm l r @(IntConstant rhv) op  = BinaryTerm l r op
+solveTerm l @(IntConstant lhv) r op  = BinaryTerm l r op
+solveTerm l r op = BinaryTerm l r op
+
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = todo
+replaceVar varName replacement expression @(BinaryTerm l r op) = 
+    let recReplaceVar = replaceVar varName replacement in
+    BinaryTerm (recReplaceVar l) (recReplaceVar r) op
+replaceVar varName replacement expression @(Variable v) | v == varName = replacement
+                                                        | otherwise = Variable v
+replaceVar varName replacement expression = expression
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate expression = todo
+evaluate expression @(BinaryTerm l r op) = solveTerm (evaluate l) (evaluate r) op
+evaluate expression = expression
--- Task1_2.hs
+++ Task1_2.hs
@@ -1,36 +1,95 @@
 module Task1_2 where
 
 import Todo(todo)
+import Prelude hiding (sin, cos, gcd)
+
+-- факториал числа (для последующих)
+factorial x = if x <= 1 then 1 else x * factorial (x - 1)
+
+--false - cos, true - sin
+taylorElement :: Double->Double->Bool->Double
+taylorElement x n t = if t then (((-1) ** n) * (x ** (2*n+1))) / factorial(2*n+1) 
+                      else  (((-1) ** n) * (x ** (2*n))) / factorial(2*n)
+--false - cos, true - sin
+taylorRecursion :: Double -> Double->Bool->Double
+taylorRecursion  x n t | x == 0 || n == -1 = 0
+          | otherwise = (taylorElement x n t) + (taylorRecursion  x (n-1) t)
+
+absXY x y | (abs x) > (abs y) = (abs x) - (abs y)
+          | (abs x) <= (abs y) = (abs y) - (abs x)
+
+frac :: Double -> Double->Double
+frac x y = absXY (x/y) (fromIntegral (floor (x/y)))
+
+--Попытка сокращать значение углов до предела от +- нуля до пи
+--Но если пи подавать через аргумент и умножать на коэффициент, например (10*pi) то пара последних разрядов побъется и пи
+--перестанет идентифицироваться как пи при рекурсивном вычитании, поэтому ниже функция sin2 закомменчена,
+--в ней обрабатывались базовые значения углов, в которых нужны точные значения, то есть pi/6, pi/2. Но даже если послать
+--10*pi, то значение не пройдет корректную конвертацию из-за того что операции с pi округляются внутри разрядной сетки,
+--поэтому функции sin и cos работают с приближенными значениями углов в заданных пределах
+convertArgument :: Double->Double
+convertArgument x |  x>0 && x>(2*pi)= convertArgument (x - 2*pi)
+                     |  x<0 && x<(-(2*pi))= convertArgument (x + 2*pi)
+                     |  x>0 && x<(2*pi) && x>(pi) = x-2*pi
+                     |  x<0 && x>(-2*pi) && x<(-pi) = x+2*pi
+                     |  x==0 = 0
+                     | otherwise = x
+
+--Версия sin, в которой обрабатываются случаи точных значений
+sin2 x = 
+    let p = (convertArgument x) in
+    if p==0 || p==pi then 0
+    else if (p==(pi/2)) then 1
+    else if (p==(3*(pi/2))) then -1
+    else if (p==((pi/6)) || p==((pi-pi/6)) ) then 0.5
+    else if (p==((-pi/6)) || p==((-pi+pi/6))) then -0.5
+    else taylorRecursion p 100 True 
+
 
 -- синус числа (формула Тейлора)
 sin :: Double -> Double
-sin x = todo
+sin x =  
+    let p = (convertArgument x) in
+    taylorRecursion p 100 True 
 
 -- косинус числа (формула Тейлора)
 cos :: Double -> Double
-cos x = todo
+cos x = 
+    let p = (convertArgument x) in
+    taylorRecursion x 100 False
 
 -- наибольший общий делитель двух чисел
 gcd :: Integer -> Integer -> Integer
-gcd x y = todo
+gcd x y = if y==0 then x
+          else gcd y (x `mod` y)
 
 -- существует ли полный целочисленный квадрат в диапазоне [from, to)?
 doesSquareBetweenExist :: Integer -> Integer -> Bool
-doesSquareBetweenExist from to = todo
+doesSquareBetweenExist from to = ((floor (sqrt (fromIntegral (to - 1)))) - (ceiling (sqrt (fromIntegral from)))) >= 0
 
 -- является ли дата корректной с учётом количества дней в месяце и
 -- вискокосных годов?
 isDateCorrect :: Integer -> Integer -> Integer -> Bool
-isDateCorrect day month year = todo
+isDateCorrect day month year | month < 1 || month > 12 || day < 1 || year == 0 = False
+  | month == 2 = day < 29 || (leap && day == 29)
+  | month `elem` [1, 3, 5, 7, 8, 10, 12] = day < 32
+  | otherwise = day < 31
+  where
+    leap = (year' `mod` 4 == 0 && year' `mod` 100 /= 0) || year' `mod` 400 == 0
+    year' | year > 0     = year
+          | otherwise = year + 1
 
 -- возведение числа в степень, duh
 -- готовые функции и плавающую арифметику использовать нельзя
 pow :: Integer -> Integer -> Integer
-pow x y = todo
+pow x y = if y==0 then 1
+               else x*(pow x (y-1))
 
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
-isPrime x = todo
+isPrime x | x == 0 = False
+          | x == 1 = False
+          | otherwise = not (any (\i -> (x `mod` i) == 0) [2..floor(sqrt(fromIntegral x))])
 
 type Point2D = (Double, Double)
 

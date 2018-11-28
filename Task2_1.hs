20ded198117d1a7b086746575fc7d4c21076b213
Pavel Pokatilo <gradergage@gmail.com>
2018-11-29 01:50:08 +0300

    Task2 fix

--- Task2_1.hs
+++ Task2_1.hs
@@ -1,7 +1,7 @@
 module Task2_1 where
 
 import Todo(todo)
-import Prelude
+import Prelude hiding(lookup)
 -- Ассоциативный массив на основе бинарного дерева поиска
 -- Ключи - Integer, значения - произвольного типа
 data TreeMap v  = Empty
@@ -13,31 +13,25 @@ emptyTree = Empty
 
 
 -- Содержится ли заданный ключ в дереве?
-contains' Empty _ = False
-contains' (Node key _ l r) k | k > key = contains' r k
-                             | k < key = contains' l k
-                             | k == key = True
-
 contains :: TreeMap v -> Integer -> Bool
-contains t k = contains' t k
+contains Empty _ = False
+contains (Node key _ l r) k | k > key = contains r k
+                            | k < key = contains l k
+                            | k == key = True
 
 -- Значение для заданного ключа
-lookup' _ Empty = error "Empty"
-lookup' k (Node key value l r) | k > key = lookup' k r
-                               | k < key = lookup' k l
-                               | k == key = value
-
 lookup :: Integer -> TreeMap v -> v
-lookup k t = lookup' k t
+lookup _ Empty = error "Empty"
+lookup k (Node key value l r) | k > key = lookup k r
+                              | k < key = lookup k l
+                              | k == key = value
 
 -- Вставка пары (ключ, значение) в дерево
-insert' (k, v) Empty = Node k v Empty Empty
-insert' (k, v) (Node key value l r) | k > key = Node key value l (insert' (k, v) r) 
-                                    | k < key = Node key value (insert' (k, v) l) r
-                                    | k == key = Node k v l r
-
 insert :: (Integer, v) -> TreeMap v -> TreeMap v
-insert (k, v) t = insert' (k, v) t
+insert (k, v) Empty = Node k v Empty Empty
+insert (k, v) (Node key value l r) | k > key = Node key value l (insert (k, v) r) 
+                                   | k < key = Node key value (insert (k, v) l) r
+                                   | k == key = Node k v l r
 
 -- Удаление элемента по ключу
 minNode :: TreeMap v -> (Integer, v)
@@ -48,17 +42,15 @@ careTree :: TreeMap v -> TreeMap v -> TreeMap v
 careTree l r =
     let (k, v) = minNode r in Node k v l (remove k r) 
 
-remove' _ Empty = error "Empty"
-remove' i (Node key value l r) | i > key = remove' i r
-                               | i < key = remove' i l
-                               | i == key = case (l, r) of
+remove :: Integer -> TreeMap v -> TreeMap v
+remove _ Empty = error "Empty"
+remove i (Node key value l r) | i > key = remove i r
+                              | i < key = remove i l
+                              | i == key = case (l, r) of
                                             (Empty, Empty) -> Empty
                                             (l , Empty) -> l
                                             (Empty, r) -> r
                                             (l, r) -> careTree l r
-
-remove :: Integer -> TreeMap v -> TreeMap v
-remove i t = remove' i t
 
 -- Поиск ближайшего снизу ключа относительно заданного
 
@@ -79,13 +71,11 @@ minKey (Node key value Empty Empty) = (key, value)
 minKey (Node key value l r) | abs'((key - getKey l)) <= abs'((key -getKey r)) = getKV l
                             | otherwise = getKV r
 
-nearestLE' k Empty = error "Empty"
-nearestLE' k node@(Node key value l r) | k > key = nearestLE' k r
-                                       | k < key = nearestLE' k l
-                                       | k == key = minKey node
-
 nearestLE :: Integer -> TreeMap v -> (Integer, v)
-nearestLE i t = nearestLE' i t
+nearestLE k Empty = error "Empty"
+nearestLE k node@(Node key value l r) | k > key = nearestLE k r
+                                      | k < key = nearestLE k l
+                                      | k == key = minKey node
 
 -- Построение дерева из списка пар
 treeFromList :: [(Integer, v)] -> TreeMap v
--- Task2_2.hs
+++ Task2_2.hs
@@ -1,7 +1,6 @@
 module Task2_2 where
 
 import Todo(todo)
-
 import Prelude hiding (foldl, foldr, unfoldr, map, concatMap, 
     filter, maxBy, minBy, reverse, sum, product, elem)
 
@@ -18,6 +17,9 @@ unfoldr f b = case f b of
                 Just (a, b') -> a : unfoldr f b'
                 Nothing -> []
 
+sizeL :: [a] -> Integer
+sizeL lst = foldl f 0 lst where
+            f acc element = acc + 1
 -- Сумма всех элементов списка (пример)
 sum :: [Integer] -> Integer
 sum lst = foldl (+) 0 lst
@@ -37,15 +39,27 @@ product :: [Integer] -> Integer
 product lst = foldl (*) 1 lst
 
 justify m lst = case m of
-                       Nothing -> lst
-                       Just v -> (v: lst)
+                     Nothing -> lst
+                     Just v -> (v: lst)
+
 -- Выделение из списка Maybe всех существующих значений
 catMaybes :: [Maybe a] -> [a]
 catMaybes lst = foldr justify [] lst
 
 -- Диагональ матрицы
+getElem n lst = foldl f 0 lst where
+                f acc elem = lst !! n
+
 diagonal :: [[a]] -> [a]
-diagonal x = zipWith (!!) x [0..]
+
+diagonal x = unfoldr f 0 where
+             f ptr | (ptr < sizeL x) = Just(((x !! (fromIntegral ptr)) !! (fromIntegral ptr)), ptr + 1)
+                   | otherwise = Nothing
+-- diagonal x = zipWith (!!) x [0..]
+-- diagonal x = unfoldr f x where
+          --   f ptr
+
+
 
 -- Фильтр для всех элементов, не соответствующих предикату
 getRes fun a lst | (fun a) = a:lst
@@ -59,12 +73,10 @@ elem :: (Eq a) => a -> [a] -> Bool
 elem e lst = (filterNot ( == e) lst) /= []
 
 -- Список чисел в диапазоне [from, to) с шагом step
-addNew a b step res | (a < b) = a:(addNew (a+step) b step res)
-                    | (a >= b) = b:res
-                    | otherwise = res
-
 rangeTo :: Integer -> Integer -> Integer -> [Integer]
-rangeTo from to step = addNew from to step []
+rangeTo from to step = unfoldr f from where
+                       f ptr | ptr < to = Just (ptr, ptr + step)
+                             | otherwise = Nothing
 
 -- Конкатенация двух списков
 append :: [a] -> [a] -> [a]
@@ -73,9 +85,22 @@ append a [] = a
 append a b = foldr f b a 
            where f t h = t:h
 
+
+
 -- Разбиение списка lst на куски размером n
 -- (последний кусок может быть меньше)
+splitList :: Integer -> [a] -> ([a], [a])
+splitList 0 t = ([], t)
+splitList _ [] = ([], [])
+splitList n (h:t) | n < 0 = ([], (h:t))
+                  | otherwise = ((h:a), b)
+                  where (a, b) = splitList (n - 1) t
 
 groups :: [a] -> Integer -> [[a]]
-groups [] _ = []
-groups lst n = (take (fromIntegral n) lst) : (groups  (drop (fromIntegral n) lst) n)
+groups t n = unfoldr f t where
+             f [] = Nothing
+             f ptr =
+                   let snd' = snd(splitList n ptr)
+                       fst' = fst(splitList n ptr) in
+                   if (sizeL snd' == 0) && (sizeL fst' == 0) then Nothing
+                   else Just (fst',snd')

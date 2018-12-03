b047ff44920a4c3b427be6aca4c1bca5cbcfd06d
Pavel Pokatilo <gradergage@gmail.com>
2018-12-03 21:37:57 +0300

    Alignment fix

--- Task3_3.hs
+++ Task3_3.hs
@@ -20,9 +20,9 @@ instance Semigroup (PSet t) where
     (<>) (PSet a) (PSet b) = PSet (\x -> a x || b x)
 
 instance Monoid (PSet a) where
-  mempty = PSet (\_ -> False) -- нейтральное значение моноида
+    mempty = PSet (\_ -> False) -- нейтральное значение моноида
   -- таким образом для PSet выполняется a `mappend` mempty === mempty `mappend` a === a
-  mappend = (<>)
+    mappend = (<>)
 
 -- для True и &&
 newtype PSet' a = PSet'(a -> Bool)
@@ -31,8 +31,8 @@ instance Semigroup (PSet' t) where
     (<>) (PSet' a) (PSet' b) = PSet' (\x -> a x && b x)
 
 instance Monoid (PSet' a) where
-  mempty = PSet' (\_ -> True)
-  mappend = (<>)
+    mempty = PSet' (\_ -> True)
+    mappend = (<>)
 
 
 -- Для функтора:
@@ -45,7 +45,7 @@ instance Monoid (PSet' a) where
 newtype СontraPSet a = СontraPSet (Bool -> a)
 
 instance Functor СontraPSet where
-   fmap f (СontraPSet a) = СontraPSet (f . a)
+    fmap f (СontraPSet a) = СontraPSet (f . a)
 --                                       ^ композиция функций
 
 -- либо применять контравариантый функтор fmap :: (a -> b) -> f b -> f a
